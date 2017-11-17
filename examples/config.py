23281bdad03283f25c73e569e6e3509b3a089921
nejni-marji <nejni.marji@gmail.com>
2017-11-17 13:46:41 -0600

    Updated the example config, to add shortcut functions, and to add more documentation

--- examples/config.py
+++ examples/config.py
@@ -6,7 +6,39 @@
 
 # I highly recommend changing these, they're really not very good defaults.
 
-# If you uncomment the 'mod' element, then the script will assume you want
+# These are some shortcut functions I use to make configuration easier.
+# I figured I'd add them into the example config.
+
+term = 'i3-sensible-term'
+shell = 'bash'
+
+# prepend ~/bin/ to the command
+def B(cmd): return '~/bin/%s' % cmd
+# prepend exec to the command (you'll probably want this more often than not)
+def E(cmd): return 'exec --no-startup-id %s' % cmd
+# prepend ~/.i3/bin/ (you probably don't have a script folder there, but I do)
+def I(cmd): return '~/.i3/bin/%s' % cmd
+# run the command in your preferred shell
+def S(cmd): return '%s -c "%s"' % (shell, cmd)
+# run the command in your preferred terminal
+def T(cmd): return '%s -e %s' % (term, cmd)
+# I have a shell script named 'remote' that does ssh and related tasks
+def R(cmd): return 'remote %s' % cmd
+# This functions basically like a goto.
+def C(wm_class): return '[class="%s"] focus' % wm_class
+
+binds: {
+	'af': E('firefox'),
+	'abcd': E('i3-sensible-term'),
+	'ap': E('pavucontrol'),
+	'at': E('telegram'),
+}
+
+# You can actually have more than one set of binds, each with a different
+# prefix key, but I figure most people wouldn't do that anyway, so this bit
+# here is a shorthand for just creating one set of binds with one prefix key.
+
+# Also, if you uncomment the 'mod' element, then the script will assume
 # key = '$mod+%s' % mod
 # sym = mod.upper()
 # The commented 'mod' elements will do exactly what is already done in the
@@ -14,32 +46,19 @@
 
 configs = [
 	{
-		'key': '$mod+m',
-		'sym': 'M',
-		#'mod': 'm'
-		'binds': {
-			'h': 'focus left',
-			'j': 'focus down',
-			'k': 'focus up',
-			'l': 'focus right',
-			'H': 'move left',
-			'J': 'move down',
-			'K': 'move up',
-			'L': 'move right',
-			'gt': 'exec telegram',
-		}
-	},
-	{
-		'key': '$mod+z',
-		'sym': 'Z',
-		#'mod': 'z',
-		'binds': {
-			'h': 'focus left',
-			'l': 'focus right',
-			'ap': 'exec pavucontrol',
-			'aP': 'exec pavucontrol',
-			'ax': 'exec xterm',
-			'abcd': 'exec xterm',
-		}
+		#'key': '$mod+Return',
+		#'sym': 'C',
+		'mod': 'Return',
+		'binds': binds
 	},
 ]
+
+# If you run `./config.py d' or `./config.py debug', it will print out all of
+# the bindings, by themselves.
+
+try:
+	if argv[1] in ['d', 'debug']:
+		for i in binds:
+			print('\'%s\': \'%s\'' % (i, binds[i]))
+except:
+	pass
